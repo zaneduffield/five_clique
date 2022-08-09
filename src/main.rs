ec61b65b1db7466082a8c8f0f9ad78c5a434352d
Zane Duffield <duffieldzane@gmail.com>
2022-08-09 23:10:23 +1000

    working! and quickly

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::io::BufRead;
 
 use itertools::Itertools;
-use rand::{seq::IteratorRandom, thread_rng};
 use rayon::prelude::*;
 use rustc_hash::FxHashMap;
 
@@ -10,7 +9,7 @@ const SLEN: usize = 5;
 
 type Word = [u8; WLEN];
 
-#[derive(Clone, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord)]
 struct LowerAsciiCharset(u32);
 impl From<Word> for LowerAsciiCharset {
     fn from(w: Word) -> Self {
@@ -34,7 +33,7 @@ impl LowerAsciiCharset {
     }
 }
 
-#[derive(Clone, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord)]
 struct Sentence<const N: usize> {
     words: [u8; N],
     len: u8,
@@ -50,26 +49,27 @@ impl<const N: usize> Sentence<N> {
         }
     }
 
-    fn add(&mut self, w: Word) -> &Self {
+    fn add(&mut self, w: Word) {
         let i = (self.len as usize) * WLEN;
         self.words[i..i + WLEN].copy_from_slice(&w);
         self.len += 1;
 
         w.iter().for_each(|b| self.charset.add(*b));
-        self
     }
 
     fn shares_chars_with(&self, w: Word) -> bool {
         self.charset.intersects(w)
     }
 
-    fn last(&self) -> Word {
-        let i = ((self.len - 1) as usize) * WLEN;
-        self.words[i..i + WLEN].try_into().unwrap()
+    fn words(&self) -> Vec<Word> {
+        self.words
+            .chunks_exact(WLEN)
+            .map(|w| w.try_into().unwrap())
+            .collect()
     }
 
     fn as_string(&self) -> String {
-        self.words.chunks_exact(WLEN).map(show).join(" ")
+        self.words().iter().map(|w| show(w)).join(" ")
     }
 }
 
@@ -77,13 +77,11 @@ fn show(bs: &[u8]) -> String {
     String::from_utf8_lossy(bs).into_owned()
 }
 
-fn show_words(ws: &[Word]) -> String {
-    ws.iter().map(|w| show(w)).join(" ")
-}
-
 fn main() {
-    let words = words();
-    let graph = build_graph(words);
+    let anagrams = anagram_groups();
+    let anagram_map = anagram_map(&anagrams);
+    let anagram_reps = anagram_map.keys().copied().collect_vec();
+    let graph = build_graph(anagram_reps);
     eprintln!("Done generating map!");
 
     let mut sols: Vec<Sentence<{ SLEN * WLEN }>> = graph
@@ -92,34 +90,80 @@ fn main() {
             let mut sols = vec![];
             let mut init = Sentence::<{ SLEN * WLEN }>::new();
             init.add(*w);
-            find(&mut sols, &graph, init, *w, nbs);
+            find_sols(&mut sols, &graph, init, *w, nbs);
             sols
         })
         .collect();
 
-    sols.sort();
-    for sol in sols {
+    let mut ana_sols = vec![];
+    for sol in &sols {
+        expand_anagrams(&mut ana_sols, &anagram_map, *sol);
+    }
+    sols.extend(ana_sols);
+
+    for sol in sols.iter().sorted() {
         println!("{}", sol.as_string());
     }
 }
 
-fn find<const N: usize>(
+fn expand_anagrams<const N: usize>(
+    sols: &mut Vec<Sentence<N>>,
+    anagram_map: &FxHashMap<Word, Vec<Word>>,
+    sol: Sentence<N>,
+) {
+    let mut a_idxs = vec![0; sol.len.into()];
+    let agrams = sol
+        .words()
+        .iter()
+        .map(|w| anagram_map.get(w).unwrap())
+        .collect_vec();
+
+    loop {
+        let mut i = 0;
+        a_idxs[0] += 1;
+        while a_idxs[i] >= agrams[i].len() {
+            a_idxs[i] = 0;
+            i += 1;
+            if i >= a_idxs.len() {
+                return;
+            }
+            a_idxs[i] += 1;
+        }
+
+        let mut sentence = Sentence::<N>::new();
+        a_idxs
+            .iter()
+            .enumerate()
+            .map(|(i, idx)| agrams[i][*idx])
+            .sorted()
+            .for_each(|w| sentence.add(w));
+
+        sols.push(sentence);
+    }
+}
+
+fn find_sols<const N: usize>(
     sols: &mut Vec<Sentence<N>>,
     graph: &WordGraph,
     cur_sol: Sentence<N>,
     last: Word,
     nbs: &[Word],
 ) {
-    for nb in nbs.iter().skip_while(|nb| nb <= &&last) {
+    let pos = match nbs.binary_search(&last) {
+        Ok(_) => nbs.len(),
+        Err(i) => i,
+    };
+
+    for nb in &nbs[pos..] {
         if cur_sol.shares_chars_with(*nb) {
             continue;
         }
-        let mut sol = cur_sol.clone();
+        let mut sol = cur_sol;
         sol.add(*nb);
         if sol.len as usize >= N / WLEN {
             sols.push(sol);
         } else {
-            find(
+            find_sols(
                 sols,
                 graph,
                 sol,
@@ -149,12 +193,7 @@ fn build_graph(words: Vec<Word>) -> WordGraph {
         .collect()
 }
 
-fn n_random(n: usize, words: &[Word]) -> Vec<Word> {
-    let mut rng = thread_rng();
-    words.iter().copied().choose_multiple(&mut rng, n)
-}
-
-fn words() -> Vec<Word> {
+fn anagram_groups() -> Vec<Vec<Word>> {
     include_bytes!("words_five.txt")
         .lines()
         .map(|line| {
@@ -164,8 +203,19 @@ fn words() -> Vec<Word> {
                 .expect("line is not the right length in bytes")
         })
         .filter(|w| distinct_letters(*w))
-        .unique_by(|w| word_chars_sorted(*w))
+        .sorted_unstable_by_key(|w| word_chars_sorted(*w))
+        .group_by(|w| word_chars_sorted(*w))
+        .into_iter()
+        .map(|(_, group)| group.collect())
         .collect()
+}
+
+fn anagram_map(anagram_groups: &[Vec<Word>]) -> FxHashMap<Word, Vec<Word>> {
+    let mut out = FxHashMap::default();
+    for group in anagram_groups {
+        out.insert(group[0], group.to_vec());
+    }
+    out
 }
 
 fn distinct_letters(w: Word) -> bool {
